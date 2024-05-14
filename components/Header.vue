e0b08a3e8141cec114208f119ed02e3fe30932e6
Rikeanggraeni <142281813+Rikeanggraeni@users.noreply.github.com>
2024-05-14 11:00:26 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -4,7 +4,7 @@
       <img src="~/assets/img/LOGO-SMK4 .png/" alt="logo SMK4">
     </div>
 
-    <div class="col-lg-10 ms-7">
+    <div class="col-lg-11">
       <h1 style="font-size: 25px; margin-top: 20px;">PERPUS DIGITAL</h1>
       <address>SMKN 4 Tasikmalaya <br />Jl.Depok, Sukamenak, Purbaratu</address>
     </div>
