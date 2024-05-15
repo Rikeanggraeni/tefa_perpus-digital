254dcfb9268ad2cf0a05d8715f74d010f49ece47
Rikeanggraeni <142281813+Rikeanggraeni@users.noreply.github.com>
2024-05-15 10:47:36 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -13,8 +13,8 @@
 
 <style scoped>
 img {
-  width: 210px;
-  height: 210px;
+  width: 200px;
+  height: 200px;
 }
 
 .h1 {
@@ -22,7 +22,7 @@ img {
   margin-left: 5px;
 }
 
-.address {
+address {
   margin-left: 5px;
 }
 </style>
